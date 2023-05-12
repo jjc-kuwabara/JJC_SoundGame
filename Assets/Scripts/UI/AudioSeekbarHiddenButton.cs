db82b8c3c3f720c5a11079ae54d5b7f57d746ca6
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-05-12 17:48:34 +0900

    seekbarを追加

---
+++ Assets/Scripts/UI/AudioSeekbarHiddenButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSeekbarHiddenButton : MonoBehaviour
+{
+    GameObject onButton;
+    GameObject offButton;
+    GameObject seekBar;
+    bool isOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        onButton = transform.Find("OnButton").gameObject;
+        offButton = transform.Find("OffButton").gameObject;
+        seekBar = GameObject.Find("AudioSeekbar");
+        isOn = false;
+        RefreshButton();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        isOn = !isOn;
+        RefreshButton();
+    }
+
+    void RefreshButton()
+    {
+        onButton.SetActive(isOn);
+        offButton.SetActive(!isOn);
+        seekBar.SetActive(isOn);
+    }
+}
